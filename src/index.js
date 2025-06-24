cdbfa90a509fbbcb40aa8be32877148509f9d942
Devashish <devashish@brilliantechsoft.com>
2025-06-24 10:56:06 +0530

    updated the allowedOrigins ports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 
-const apiRoutes = require('./routes'); 
+const apiRoutes = require('./routes');
 const adminRoutes = require('./routes/v1/admin-routes');
 const { ServerConfig } = require('./config');
 const userRoutes = require('./routes/v1/user-routes');
@@ -15,32 +15,32 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const allowedOrigins = [
-  'http://139.59.23.86',
-  'http://localhost:5173',
-   'http://localhost:5174',
-  'http://127.0.0.1:3000'
+    'http://139.59.23.86',
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'http://127.0.0.1:3000'
 ];
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
 }));
 
 
 app.use('/admin', adminRoutes);
-app.use('/api',apiRoutes)
+app.use('/api', apiRoutes)
 app.use(errorHandler);
 app.use('/api', deviceRoutes);
 
 
 
 app.listen(process.env.PORT, () => {
-console.log(`Server is running on port ${process.env.PORT}`);
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
