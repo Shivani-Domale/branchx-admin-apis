09ca0131c8a3d0665eee2efdf9a2e6602fe03150
Devashish <devashish@brilliantechsoft.com>
2025-06-24 10:55:13 +0530

    updated the allowedOrigins ports

--- src/index.js
+++ src/index.js
@@ -14,12 +14,25 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const allowedOrigins = [
+  'http://139.59.23.86',
+  'http://localhost:5173',
+   'http://localhost:5174',
+  'http://127.0.0.1:3000'
+];
 app.use(cors({
-    origin: 'http://localhost:5173', // Allow all origins
-    methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'], // Allow specific HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'],// Allow specific headers
-    credentials:true // Allow credentials (cookies, authorization headers, etc.)
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 }));
+
 
 app.use('/admin', adminRoutes);
 app.use('/api',apiRoutes)
